61cca4fd8fb41084c5fab6867fd30818ef1e4502
Daniel Kim <daniel.seungchul.kim@gmail.com>
2019-11-13 19:12:26 -0500

    added test for k6

---
+++ script.js
@@ -0,0 +1,19 @@
+import http from "k6/http";
+import { check, sleep } from "k6";
+
+export let options = {
+  stages: [
+    { duration: "30s", target: 20 },
+    { duration: "1m30s", target: 10 },
+    { duration: "20s", target: 0 }
+  ]
+};
+
+export default function() {
+  let res = http.get("http://localhost:3000/products/1/styles");
+  check(res, {
+    "status was 200": r => r.status == 200,
+    "transaction time OK": r => r.timings.duration < 200
+  });
+  sleep(1);
+}
