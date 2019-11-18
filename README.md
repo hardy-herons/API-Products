fd3418de0d1629f44d8f5a8987c81f38aac0ebd5
Daniel Kim <daniel.seungchul.kim@gmail.com>
2019-11-18 14:12:55 -0500

    updated with redis

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
+# ORDER OF DEVELOPMENT
+
+### test on localmachine
+
+### docker-compose
+
+### reinitialize docker db (for now, subject to change)
+
+#### migrate the db, load the database
+
+- ONLY IF YOU DOWN YOUR DOCKER
+
+### push to github when good
+
+### ssh into aws ec2 instance and pull from github
+
+### take down the docker-compose within the ec2
+
+### redo whole process.
+
 # API-Products
 
 using python to "transform" my data from the csv files
@@ -23,9 +43,17 @@ create index photo_style on public."Photos" ("styleId");
 create index product_style on public."Styles" ("productId");
 create index sku_style on public."SKUs" ("styleId");
 
+# INSTRUCTIONS FOR AWS EC2 AND DOCKER
+
 COPY public."Related" FROM '/var/lib/postgresql/data/pgdata/transformedrelated.csv' WITH DELIMITER '|' CSV HEADER;  
 COPY public."Photos" FROM '/var/lib/postgresql/data/pgdata/transformedphotos.csv' WITH DELIMITER '|' CSV HEADER;  
 COPY public."SKUs" FROM '/var/lib/postgresql/data/pgdata/transformedskus.csv' WITH DELIMITER '|' CSV HEADER;  
 COPY public."Products" FROM '/var/lib/postgresql/data/pgdata/transformedProduct.csv' WITH DELIMITER '|' CSV HEADER;  
 COPY public."Styles" FROM '/var/lib/postgresql/data/pgdata/transformedstyles.csv' WITH DELIMITER '|' CSV HEADER;  
 COPY public."Features" FROM '/var/lib/postgresql/data/pgdata/transformedfeatures.csv' WITH DELIMITER '|';
+
+psql postgres://danielkim@localhost:35432/products
+INSIDE AWS FOR DOCKERS pg
+sudo docker-compose run postgres psql -h postgres -U postgres
+
+http://18.216.155.150/
--- server.js
+++ server.js
@@ -1,13 +1,42 @@
 const express = require("express");
+const redis = require("redis");
 const { Product, Related, Feature, Style, Photo, SKU } = require("./models");
 
 const app = express();
+let client;
+if (process.env.REDIS_URL) {
+  client = redis.createClient(process.env.REDIS_URL);
+} else {
+  client = redis.createClient(6379);
+}
+
+// echo redis errors to the console
+client.on("error", err => {
+  console.log("Error: " + err);
+});
+
+app.get("/loaderio-bb60c0a8dc0c0eaf8d83f532a2dda91f.html", (req, res) => {
+  res.send("loaderio-bb60c0a8dc0c0eaf8d83f532a2dda91f");
+});
 
 app.get("/products/list", async (req, res) => {
   //TODO: the additional queries for page and count
   try {
-    const products = await Product.findAll({ limit: 5 });
-    res.json(products);
+    const productsListKey = "products:list";
+
+    return client.get(productsListKey, (err, productsList) => {
+      if (productsList) {
+        return res.json(JSON.parse(productsList));
+      } else {
+        return Product.findAll({ limit: 5 })
+          .tap(products => {
+            client.setex(productsListKey, 3600, JSON.stringify(products));
+          })
+          .then(products => {
+            res.json(products);
+          });
+      }
+    });
   } catch (e) {
     res.status(500).json(e.message);
   }
@@ -16,13 +45,21 @@ app.get("/products/list", async (req, res) => {
 /* id, name, slogan, description, category, default_price, features[{}] */
 app.get("/products/:product_id", async (req, res) => {
   try {
-    const product = await Product.findByPk(req.params.product_id);
-    const features = await Feature.findAll({
-      where: { product_id: req.params.product_id },
-      attributes: ["feature", "value"]
+    const idDetailKey = `${req.params.product_id}:details`;
+    return client.get(idDetailKey, async (err, product) => {
+      if (product) {
+        return res.json(JSON.parse(product));
+      } else {
+        const product = await Product.findByPk(req.params.product_id);
+        const features = await Feature.findAll({
+          where: { product_id: req.params.product_id },
+          attributes: ["feature", "value"]
+        });
+        product.dataValues["features"] = features;
+        client.setex(idDetailKey, 3600, JSON.stringify(product));
+        res.json(product);
+      }
     });
-    product.dataValues["features"] = features;
-    res.json(product);
   } catch (e) {
     res.status(500).json(e.message);
   }
@@ -31,44 +68,52 @@ app.get("/products/:product_id", async (req, res) => {
 /* style_id, name, original_price, sale_price, default, photos[{}], skus{}*/
 app.get("/products/:product_id/styles", async (req, res) => {
   try {
-    const results = {
-      product_id: req.params.product_id,
-      results: []
-    };
-    const styles = await Style.findAll({
-      where: {
-        productId: req.params.product_id
-      },
-      attributes: { exclude: ["productId"] }
-    });
+    const idStyleKey = `${req.params.product_id}:styles`;
+    return client.get(idStyleKey, async (err, styles) => {
+      if (styles) {
+        return res.json(JSON.parse(styles));
+      } else {
+        const results = {
+          product_id: req.params.product_id,
+          results: []
+        };
+        const styles = await Style.findAll({
+          where: {
+            productId: req.params.product_id
+          },
+          attributes: { exclude: ["productId"] }
+        });
 
-    for (let i = 0; i < styles.length; i++) {
-      const style = styles[i].dataValues;
-      const temp = {
-        style_id: style.id,
-        name: style.name,
-        original_price: style.original_price,
-        sale_price: style.sale_price,
-        "default?": style.default_style
-      };
-      const photos = await Photo.findAll({
-        where: { styleId: styles[i].dataValues.id },
-        attributes: { exclude: ["id", "styleId"] }
-      });
-      temp["photos"] = photos;
-      const skus = await SKU.findAll({
-        where: { styleId: styles[i].dataValues.id },
-        attributes: { exclude: ["id", "styleId"] }
-      });
-      const skusObj = skus.reduce((acc, cur) => {
-        const { dataValues } = cur;
-        acc[dataValues.size] = dataValues.quantity;
-        return acc;
-      }, {});
-      temp["skus"] = skusObj;
-      results.results.push(temp);
-    }
-    res.json(results);
+        for (let i = 0; i < styles.length; i++) {
+          const style = styles[i].dataValues;
+          const temp = {
+            style_id: style.id,
+            name: style.name,
+            original_price: style.original_price,
+            sale_price: style.sale_price,
+            "default?": style.default_style
+          };
+          const photos = await Photo.findAll({
+            where: { styleId: styles[i].dataValues.id },
+            attributes: { exclude: ["id", "styleId"] }
+          });
+          temp["photos"] = photos;
+          const skus = await SKU.findAll({
+            where: { styleId: styles[i].dataValues.id },
+            attributes: { exclude: ["id", "styleId"] }
+          });
+          const skusObj = skus.reduce((acc, cur) => {
+            const { dataValues } = cur;
+            acc[dataValues.size] = dataValues.quantity;
+            return acc;
+          }, {});
+          temp["skus"] = skusObj;
+          results.results.push(temp);
+        }
+        client.setex(idStyleKey, 3600, JSON.stringify(results));
+        res.json(results);
+      }
+    });
   } catch (e) {
     res.status(500).json(e.message);
   }
@@ -77,17 +122,25 @@ app.get("/products/:product_id/styles", async (req, res) => {
 /* array of related ids */
 app.get("/products/:product_id/related", async (req, res) => {
   try {
-    const related = await Related.findAll({
-      where: {
-        current_product_id: req.params.product_id
-      },
-      attributes: ["related_product_id"]
+    const idRelatedKey = `${req.params.product_id}:related`;
+    return client.get(idRelatedKey, async (err, related) => {
+      if (related) {
+        return res.json(JSON.parse(related));
+      } else {
+        const related = await Related.findAll({
+          where: {
+            current_product_id: req.params.product_id
+          },
+          attributes: ["related_product_id"]
+        });
+        const results = [];
+        for (let i = 0; i < related.length; i++) {
+          results.push(related[i]["related_product_id"]);
+        }
+        client.setex(idRelatedKey, 3600, JSON.stringify(results));
+        res.json(results);
+      }
     });
-    const results = [];
-    for (let i = 0; i < related.length; i++) {
-      results.push(related[i]["related_product_id"]);
-    }
-    res.json(results);
   } catch (e) {
     res.status(500).json(e.message);
   }
